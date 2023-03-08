90a1d83bed13a9c2939cc00f04a177425dfbb215
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-08 21:25:30 +0200

    logs check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,29 @@
 // import { useState } from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import { FaSistrix } from "react-icons/fa";
 // import { fetchFilterCharacters } from "../charactersAPI";
 
 const SearchBar = ({ onSearch }) => {
 
-    const [query, setQuery] = useState("");
+    // const [query, setQuery] = useState("");
 
     const handleChange = e => {
         const newSearchValue = e.target.value.toLowerCase();
-        setQuery(newSearchValue);
+        // setQuery(newSearchValue);
+        onSearch(newSearchValue)
     }
 
-    const handleSubmit = e => {
-        console.log(e);
-        e.preventDefault();
-        if (query.trim() === '') {
-            return;
-        }
-        onSearch(query)
-    }
+    // const handleSubmit = e => {
+    //     console.log(e);
+    //     e.preventDefault();
+    //     if (query.trim() === '') {
+    //         return;
+    //     }
+    //     onSearch(query)
+    // }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form>
             <div className="search-input-wrapper">
                 <FaSistrix size={18} color="rgba(0, 0, 0, 0.54)" />
                 <input className="search-input" type="text" onChange={handleChange} placeholder="Filter by name..." />
